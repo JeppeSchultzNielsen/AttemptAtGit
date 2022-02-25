7c99312c9989abe05c9bc669cb36a7873643a982
Jeppe Schultz Nielsen <jeppe-schultz@hotmail.com>
2022-02-25 15:01:42 +0100

    Nu har jeg lavet flere ændringer

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Nu har jeg lavet ændringer" << std::endl;
+    std::cout << "Nu har jeg lavet flere ændringer" << std::endl;
     return 0;
 }
